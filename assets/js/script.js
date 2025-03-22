d368881ea0620dba0b4da80591ee95df9ff7d177
Vinicius Dizatnikis <vdizatnikis@gmail.com>
2025-03-21 22:39:19 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+let temaEscuro = true;  // Inicia no tema escuro
+
+document.addEventListener("DOMContentLoaded", function () {
+    const selector = document.getElementById("theme-toggle");
+
+    // Definir o tema escuro logo ao carregar a página
+    if (temaEscuro) {
+        document.documentElement.style.setProperty('--text-color', '#ffffff');
+        document.documentElement.style.setProperty('--primary-color', '#292727');
+        document.documentElement.style.setProperty('--image', "url('../img/backgroundDark.jpg')");
+    }
+
+    selector.addEventListener("click", function () {
+        temaEscuro = !temaEscuro;  // Inverte o estado do tema
+
+        if (temaEscuro) {
+            // Tema escuro
+            document.documentElement.style.setProperty('--text-color', '#ffffff');
+            document.documentElement.style.setProperty('--primary-color', '#292727');
+            document.documentElement.style.setProperty('--secundary-color', '#383737');
+            document.documentElement.style.setProperty('--image', "url('../img/backgroundDark.jpg')");
+        } else {
+            // Tema claro
+            document.documentElement.style.setProperty('--primary-color', '#ffffff');
+            document.documentElement.style.setProperty('--text-color', '#000000');
+            document.documentElement.style.setProperty('--secundary-color', '#f0ecec');
+            document.documentElement.style.setProperty('--image', "url('../img/backgroundLigth.jpg')");
+        }
+
+        this.classList.toggle("active");
+    });
+});
