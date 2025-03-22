225bef5a97432bbdafecf5ff2b81ee8b1fed947c
Vinicius Dizatnikis <vdizatnikis@gmail.com>
2025-03-22 20:18:21 -0300

    update contact and others

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 AOS.init({
-    duration: 1000,    
-    once: true,          
-    offset: 300,     
+    duration: 1000,
+    once: true,
+    offset: 300,
     anchor: 'top-center',
-    easing: 'ease-in-out', 
-    delay: 100,         
+    easing: 'ease-in-out',
+    delay: 100,
 });
 
 
-let temaEscuro = true;  
+let temaEscuro = true;
 
 document.addEventListener("DOMContentLoaded", function () {
     const selector = document.getElementById("theme-toggle");
@@ -26,16 +26,42 @@ document.addEventListener("DOMContentLoaded", function () {
             // Tema escuro
             document.documentElement.style.setProperty('--text-color', '#ffffff');
             document.documentElement.style.setProperty('--primary-color', '#292727');
-            document.documentElement.style.setProperty('--secundary-color', '#383737');
+            document.documentElement.style.setProperty('--secondary-color', '#383737');
             document.documentElement.style.setProperty('--image', "url('../img/backgroundDark.jpg')");
         } else {
             // Tema claro
             document.documentElement.style.setProperty('--primary-color', '#ffffff');
             document.documentElement.style.setProperty('--text-color', '#000000');
-            document.documentElement.style.setProperty('--secundary-color', '#f0ecec');
+            document.documentElement.style.setProperty('--secondary-color', '#f0ecec');
             document.documentElement.style.setProperty('--image', "url('../img/backgroundLigth.jpg')");
         }
 
         this.classList.toggle("active");
     });
+});
+
+(function () {
+    emailjs.init("0NzRgS6NvyBq2d7AD"); 
+})();
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+
+    const formData = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+
+    emailjs.send("service_lked5ls", "template_3vgr7lc", formData)
+        .then(response => {
+            document.getElementById("response-message").innerText = "E-mail enviado com sucesso!";
+            document.getElementById("contact-form").reset(); 
+        })
+        .catch(error => {
+            document.getElementById("response-message").innerText = "Erro ao enviar o e-mail.";
+            console.error("Erro:", error);
+        });
 });
