7e0923daba03814b43226c6eae634843b1ef1af5
Vinicius Dizatnikis <vdizatnikis@gmail.com>
2025-05-25 13:14:28 -0300

    update v2

--- app/js/script.js
+++ app/js/script.js
@@ -15,12 +15,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.getElementById("menu-bar");
     const mobileMenu = document.getElementById("mobile-menu");
     const closeMenu = document.getElementById("close-menu");
-    
+
     function atualizarTema() {
         document.documentElement.style.setProperty('--text-color', temaEscuro ? '#ffffff' : '#000000');
         document.documentElement.style.setProperty('--primary-color', temaEscuro ? '#292727' : '#ffffff');
         document.documentElement.style.setProperty('--secondary-color', temaEscuro ? '#383737' : '#f0ecec');
-        document.documentElement.style.setProperty('--image', temaEscuro ? "url('../img/backgroundDark.jpg')" : "url('../img/backgroundLigth.jpg')");
+        document.documentElement.style.setProperty('--image', temaEscuro ? "url('../../assets/img/backgrounds/backgroundDark.jpg')" : "url('../../assets/img/backgrounds/backgroundLigth.jpg')");
+        document.querySelectorAll('.mysql img').forEach(img => {
+            img.src = temaEscuro ? 'assets/icons/tech/mysqlWhite.png' : 'assets/icons/tech/mysqlBlack.png';
+        });
+
+        document.querySelectorAll('.tailwind img').forEach(img => {
+            img.src = temaEscuro ? 'assets/icons/tech/tailwindWhite.png' : 'assets/icons/tech/tailwindBlack.png';
+        });
+
+        document.querySelectorAll('.expressJs img').forEach(img => {
+            img.src = temaEscuro ? 'assets/icons/tech/expressWhite.png' : 'assets/icons/tech/expressBlack.png';
+        });
+
+        document.querySelectorAll('.prisma img').forEach(img => {
+            img.src = temaEscuro ? 'assets/icons/tech/prismaWhite.png' : 'assets/icons/tech/prismaBlack.png';
+        });
+
+        document.querySelectorAll('.mongoDb img').forEach(img => {
+            img.src = temaEscuro ? 'assets/icons/tech/mongoDbWhite.png' : 'assets/icons/tech/mongoDbBlack.png';
+        });
     }
 
     function alternarTema() {
@@ -35,12 +54,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     atualizarTema();
-    
+
     selectorDesktop.addEventListener("click", alternarTema);
     selectorMobile.addEventListener("click", alternarTema);
     menuToggle.addEventListener("click", () => mobileMenu.classList.toggle("active"));
     closeMenu.addEventListener("click", fecharMenu);
-    
+
     document.querySelectorAll(".mobile-menu .header__link").forEach(link => {
         link.addEventListener("click", fecharMenu);
     });
@@ -59,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function () {
 document.getElementById("contact-form").addEventListener("submit", function (event) {
     event.preventDefault();
     const responseMessage = document.getElementById("response-message");
-    
+
     const formData = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
@@ -75,6 +94,6 @@ document.getElementById("contact-form").addEventListener("submit", function (eve
             responseMessage.innerText = "Erro ao enviar o e-mail. Tente novamente mais tarde.";
             console.error("Erro:", error);
         });
-    
+
     responseMessage.scrollIntoView({ behavior: 'smooth' });
 });
